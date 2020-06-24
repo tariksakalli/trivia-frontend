b0672d1ef3f4479f298b02a6aaa328a22b7678f3
Tarık Sakallı <tarik.sakalli@outlook.com>
2020-06-24 03:29:25 +0300

    Update StudentResults.vue
    Add test answers.

--- src/views/Admin/StudentResults.vue
+++ src/views/Admin/StudentResults.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <b-table :items="filterResults" dark></b-table>
+    <b-table :fields="fields" :items="filterResults" dark>
+      <template v-slot:cell(answers)="data">
+        <b-table :fields="answerFields" :items="JSON.parse(data.item.answers)" dark>
+        </b-table>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -12,6 +17,21 @@ export default {
   data() {
     return {
       studentId: this.$route.params.id,
+      testAnswers: [],
+      fields: [
+        { key: 'idtests', label: 'ID' },
+        { key: 'studentname', label: 'İsim' },
+        { key: 'testname', label: 'Test' },
+        { key: 'date', label: 'Tarih' },
+        { key: 'totaltime', label: 'Süre' },
+        { key: 'answers', label: 'Cevaplar' },
+      ],
+      answerFields: [
+        { key: 'level', label: 'Seviye' },
+        { key: 'answer', label: 'Cevap' },
+        { key: 'duration', label: 'Süre' },
+        { key: 'correctAnswer', label: 'Doğru Cevap' },
+      ],
     };
   },
   computed: {
@@ -21,10 +41,7 @@ export default {
     filterResults() {
       return this.testResults.map((item) => ({
         ...item,
-      })).filter((item) => {
-        console.log(item.answers);
-        return item.studentname === this.studentId;
-      });
+      })).filter((item) => item.studentname === this.studentId);
     },
   },
 };
